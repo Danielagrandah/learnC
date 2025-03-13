b0886c1fc7ee5076f314b1e16f6180a285c023ab
Daniela Granda Ahumada <karen.grandah@gmail.com>
2025-03-13 13:22:46 +0100

    Create myfirstprogram.c
    Hello world!

---
+++ myfirstprogram.c
@@ -0,0 +1,12 @@
+#include <studio.h> /*header file library that lets us work with input and output functions*/
+
+/*main() is a function*/
+/*printf is a function to outprint/print text*/
+/*return0 ends the main fuction*/
+/*{the funtcion goes inside}*/
+/*the staments(instructions) always end with a semicolon*/
+/* (\n) is called an escape sequence*/
+int main(){
+  printf("Hello world!\nI am learning C.\nAnd it is awesome!");
+  return 0;
+}
