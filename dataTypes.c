502cc793af457bd0b041f6736c80a03330245245
Daniela Granda Ahumada <karen.grandah@gmail.com>
2025-03-13 14:54:30 +0100

    Create dataTypes.c

---
+++ dataTypes.c
@@ -0,0 +1,11 @@
+// Create variables
+int myNum = 5;             // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'D';       // Character
+
+// Print variables
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter); 
+
+//Note that we use the %lu format specifer to print the result, instead of %d. It is because the compiler expects the sizeof operator to return a long unsigned int (%lu), instead of int (%d). On some computers it might work with %d, but it is safer to use %lu.
